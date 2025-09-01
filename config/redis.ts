54963010daa725225f6dced69add7dd816933aac
Lalit Adhikari <lalit.adhikari@devslane.com>
2025-09-01 18:45:00 +0530

    Configured redis tls

--- config/redis.ts
+++ config/redis.ts
@@ -1,23 +1,30 @@
 import { defineConfig } from '@adonisjs/redis'
-import { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT } from '../app/utils/secret.js'
+import { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT, REDIS_TLS_ENABLED } from '../app/utils/secret.js'
+
+const getMainConnection = () => {
+  const mainConnection = {
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    password: REDIS_PASSWORD,
+    db: 0,
+    keyPrefix: '',
+
+    maxRetriesPerRequest: null,
+    enableReadyCheck: false,
+    retryStrategy(times: number) {
+      return Math.min(times * 50, 2000) // reconnect every 50ms up to 2s
+    },
+  } as any
+  if (REDIS_TLS_ENABLED) {
+    mainConnection.tls = {}
+  }
+  return mainConnection
+}
 
 const redisConfig = defineConfig({
   connection: 'main',
   connections: {
-    main: {
-      host: REDIS_HOST,
-      port: REDIS_PORT,
-      password: REDIS_PASSWORD,
-      db: 0,
-      keyPrefix: '',
-      // tls: {},
-
-      maxRetriesPerRequest: null,
-      enableReadyCheck: false,
-      retryStrategy(times: number) {
-        return Math.min(times * 50, 2000) // reconnect every 50ms up to 2s
-      },
-    },
+    main: getMainConnection(),
   },
 })
 
